27590e0996bbcde26d5016bdd30089efe29d7bba
Michael Smith <michael.smith@puppetlabs.com>
2015-07-21 16:07:18 -0700

    (maint) Move CURL_STATIC to Leatherman
    Without this change, no option is provided within Leatherman to link
    against a static build of libcurl. On Windows, setting CURL_STATICLIB is
    required to build against a static build of libcurl to resolve symbols.
    
    Move the CURL_STATIC option and basic configuration from Facter to
    Leatherman.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,6 +1,7 @@
 include(leatherman)
 defoption(COVERALLS "Generate code coverage using Coveralls.io" OFF)
 defoption(BOOST_STATIC "Use Boost's static libraries" OFF)
+defoption(CURL_STATIC "Use curl's static libraries" OFF)
 
 # Map our boost option to the for-realsies one
 set(Boost_USE_STATIC_LIBS ${BOOST_STATIC})
