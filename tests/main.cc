537a5180c5103559148e35257bc514fd99ca2500
Michael Smith <michael.smith@puppetlabs.com>
2015-09-17 13:00:26 -0700

    (maint) Add code to enable logging in tests
    Adds ifdef code to enable logging tests. This can be useful for debugging
    tests.

--- tests/main.cc
+++ tests/main.cc
@@ -1,7 +1,17 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+// To enable log messages:
+// #define ENABLE_LOGGING
+
+#ifdef ENABLE_LOGGING
+#include <leatherman/logging/logging.hpp>
+#endif
+
 int main(int argc, char **argv)
 {
+#ifdef ENABLE_LOGGING
+    leatherman::logging::set_level(leatherman::logging::log_level::debug);
+#endif
     return Catch::Session().run( argc, argv );
 }
