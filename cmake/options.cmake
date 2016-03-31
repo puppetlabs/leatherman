c6812542abcef3338a4f2c2821f3ce9034463b54
Michael Smith <michael.smith@puppetlabs.com>
2016-03-31 16:16:36 -0700

    (LTH-74) Switch BOOST_STATIC to ON by default
    Our packaging builds with static Boost libraries. Switch the Leatherman
    default to do the same so developer builds are more consistent with
    packaging builds.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,6 +1,6 @@
 include(leatherman)
 defoption(COVERALLS "Generate code coverage using Coveralls.io" OFF)
-defoption(BOOST_STATIC "Use Boost's static libraries" OFF)
+defoption(BOOST_STATIC "Use Boost's static libraries" ON)
 defoption(CURL_STATIC "Use curl's static libraries" OFF)
 set(LIB_SUFFIX "" CACHE STRING "Library install suffix")
 
