9b1e207e649ef23499aa81992e7badfa891a9d47
Michael Smith <michael.smith@puppetlabs.com>
2016-04-18 16:12:41 -0700

    (LTH-93) Support building on Nano Server
    Make adds `-lkernel32` to the list of link libraries by default. However,
    doing so with MinGW-w64 introduces an unsatisfied dependency on Nano
    Server, causing any executable to fail to run. Explicitly set default link
    libraries for all C++ projects to avoid linking kernel32.

--- cmake/cflags.cmake
+++ cmake/cflags.cmake
@@ -66,6 +66,13 @@ if (COVERALLS)
 endif()
 
 if (WIN32)
+    # Update standard link libraries to explicitly exclude kernel32. It isn't necessary, and when compiling with
+    # MinGW makes the executable unusable on Microsoft Nano Server due to including __C_specific_handler.
+    SET(CMAKE_C_STANDARD_LIBRARIES "-luser32 -lgdi32 -lwinspool -lshell32 -lole32 -loleaut32 -luuid -lcomdlg32 -ladvapi32"
+        CACHE STRING "Standard C link libraries." FORCE)
+    SET(CMAKE_CXX_STANDARD_LIBRARIES "-luser32 -lgdi32 -lwinspool -lshell32 -lole32 -loleaut32 -luuid -lcomdlg32 -ladvapi32"
+        CACHE STRING "Standard C++ link libraries." FORCE)
+
     # We currently support Windows Server 2003, which requires using deprecated APIs.
     # See http://msdn.microsoft.com/en-us/library/windows/desktop/aa383745(v=vs.85).aspx for version strings.
     # When Server 2003 support is discontinued, the networking facts implementation can be cleaned up, and
