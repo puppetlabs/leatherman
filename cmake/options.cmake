0befd41c12d6719080e9d24393bd630ef9d52a71
Michael Smith <michael.smith@puppetlabs.com>
2016-04-22 13:02:26 -0700

    (maint) Make prefixing project includes the default
    Ensures project include files are always preferred over system include
    files, avoiding the problem of picking up headers from an installed copy
    of Leatherman while building.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,6 +2,7 @@ include(leatherman)
 defoption(COVERALLS "Generate code coverage using Coveralls.io" OFF)
 defoption(BOOST_STATIC "Use Boost's static libraries" OFF)
 defoption(CURL_STATIC "Use curl's static libraries" OFF)
+set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON CACHE BOOL "Prepend project includes before system includes")
 set(LIB_SUFFIX "" CACHE STRING "Library install suffix")
 
 # Solaris and AIX have poor support for std::locale and boost::locale
