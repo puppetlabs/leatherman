fc63f6596243664b7a062a654d58427591e81121
Michael Smith <michael.smith@puppet.com>
2016-06-02 17:44:47 -0700

    (maint) Fix compilation with LEATHERMAN_USE_LOCALES=OFF
    Code that's only compiled when `LEATHERMAN_USE_LOCALES` is OFF was
    incorrect. Fix it and make it more consistent with other code by `using
    namespace std`.
    
    Add a Travis run to test this code path.

--- locale/disabled/locale.cc
+++ locale/disabled/locale.cc
@@ -2,18 +2,20 @@
 
 namespace leatherman { namespace locale {
 
-    const std::locale get_locale(std::string const& id, std::string const& domain, std::vector<std::string> const& paths)
+    using namespace std;
+
+    const std::locale get_locale(string const& id, string const& domain, vector<string> const& paths)
     {
         // std::locale is not supported on these platforms
-        throw std::runtime_error("leatherman::locale::get_locale is not supported on this platform");
+        throw runtime_error("leatherman::locale::get_locale is not supported on this platform");
     }
 
-    void clear_domain(std::string const& domain)
+    void clear_domain(string const& domain)
     {
-        throw std::runtime_error("leatherman::locale::clear_domain is not supported on this platform");
+        throw runtime_error("leatherman::locale::clear_domain is not supported on this platform");
     }
 
-    std::string translate(std::string const& msg, std::string const& domain)
+    string translate(string const& msg, string const& domain)
     {
         return msg;
     }
